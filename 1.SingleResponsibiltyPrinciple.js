fa9e2aa68545b3fbe45616404e31659826d93bfd
Stephen Pino <Stephenp3123@gmail.com>
2022-07-25 18:01:37 +0100

    module import path amended

---
+++ 1.SingleResponsibiltyPrinciple.js
@@ -0,0 +1,12 @@
+import {printReport} from "./modules/printReport";
+
+// “There should never be more than one reason for a class to change.”
+// Every function you write should do exactly one thing. It should have one clearly defined goal.
+
+//Just because you can doesn't mean you should. 
+
+printReport();
+
+
+
+
