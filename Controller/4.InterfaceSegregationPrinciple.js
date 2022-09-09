338b91f83bd7f45967be7f1a687ebdfee9ac6ef4
Stephen Pino <Stephenp3123@gmail.com>
2022-09-09 16:26:43 +0100

    SoC

---
+++ Controller/4.InterfaceSegregationPrinciple.js
@@ -0,0 +1,13 @@
+import { Penguin } from "../modules/4.InterfaceSegregationPrinciple/Penguin.js"; 
+import { Bird } from "../modules/4.InterfaceSegregationPrinciple/Bird.js"; 
+import { flyer } from "../modules/4.InterfaceSegregationPrinciple/Flyer.js"; 
+
+
+//Good example penuin should error...
+Object.assign(Bird.prototype, flyer);
+
+const bird = new Bird();
+bird.fly(); // Outputs 'Flap flap, I'm flying!'
+
+const penguin = new Penguin();
+penguin.fly(); // 'Error: penguin.fly is not a function'
