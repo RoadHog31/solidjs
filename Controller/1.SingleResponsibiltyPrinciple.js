862bd1bcd3ca20110de60e438a68238a82363a0b
Stephen Pino <Stephenp3123@gmail.com>
2022-11-25 09:49:11 +0000

    SRP code updates.

--- Controller/1.SingleResponsibiltyPrinciple.js
+++ Controller/1.SingleResponsibiltyPrinciple.js
@@ -1,4 +1,4 @@
-import {Report} from "./modules/printReport";
+import {Report} from "./modules/printReport.js";
 import { Employee } from "./modules/employee";
 import {Automobile} from "../modules/1.SingleResponsibiltyPrinciple/automobile";
 // “There should never be more than one reason for a class to change.”
@@ -9,7 +9,7 @@ import {Automobile} from "../modules/1.SingleResponsibiltyPrinciple/automobile";
 //This does not mean that the Report class should only have one method, it can have many methods but it should only have one reason to change.
 
 
-
+console.log("Hi!");
 
 
 
